6056ae859816a887837d4c33312092862dbc18e9
wodim <neikokz@gmail.com>
2012-03-17 13:31:33 +0100

    database structure

--- sql/quotes.sql
+++ sql/quotes.sql
@@ -1,14 +1,22 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `quotes`
+--
+
+--
+-- Table structure for table `quotes`
+--
 
 CREATE TABLE IF NOT EXISTS `quotes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permaid` varchar(4) NOT NULL,
   `nick` varchar(20) NOT NULL,
   `date` datetime NOT NULL,
   `ip` varchar(32) NOT NULL,
@@ -22,8 +30,19 @@ CREATE TABLE IF NOT EXISTS `quotes` (
   `approved` int(1) NOT NULL DEFAULT '0',
   `hidden` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=599 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nick` varchar(16) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `salt` varchar(8) NOT NULL,
+  `db` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;
