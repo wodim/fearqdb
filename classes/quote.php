737715eb9fa3c40eb41e16fd1066e98d992f4799
wodim <neikokz@gmail.com>
2012-07-20 05:16:17 +0200

    Search criteria highlighting is fixed... again

--- classes/quote.php
+++ classes/quote.php
@@ -165,6 +165,13 @@ class Quote {
 		return true;
 	}
 
+	private function translit_fix($text) {
+		return str_replace(
+			array('«', '»', '€'),
+			array('<', '>', 'e'),
+			$text);
+	}
+
 	private function text_clean($text, $for = 'www_body') {
 		// this is real crap.
 
@@ -173,6 +180,31 @@ class Quote {
 		// clean only for www. rss uses the CDATA structure which does not require escaping
 		if ($for == 'www_body' || $for == 'www_comment') {
 			$text = htmlspecialchars($text);
+		}
+
+		// mark the search criterium
+		if ($for == 'www_body' && $session->search) {
+			$pos = 0;
+			$length = mb_strlen($session->search);
+			$criteria = mb_strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $session->search));
+			do {
+				// unfortunately we have to do this each time...
+				$plain = mb_strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $this->translit_fix($text)));
+				$offset = mb_strpos($plain, $criteria, $pos);
+				if ($offset === false) {
+					break;
+				}
+				$first = '<strong class="criteria">';
+				$last = '</strong>';
+
+				$text = sprintf('%s%s%s%s%s',
+					mb_substr($text, 0, ($offset)),
+					$first,
+					mb_substr($text, $offset, $length),
+					$last,
+					mb_substr($text, ($offset + $length)));
+				$pos = $offset + strlen($first) + $length + strlen($last);
+			} while (1);
 		}
 
 		// clean special chars from copypasting from irc
