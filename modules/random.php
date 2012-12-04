079aa1d25b61f06453fc6925ea1f795f6bc1ee3c
wodim <neikokz@gmail.com>
2012-12-04 16:08:15 +0100

    filter random quotes by time lapses

--- modules/random.php
+++ modules/random.php
@@ -19,18 +19,66 @@
 
 require(classes_dir.'quote.php');
 
+$selected = new stdClass();
+$beacon = 'class="selected" ';
+
+if (!isset($params[1]) ||
+	!isset($params[2]) && $params[1] != 'all') {
+	redir(sprintf('%srandom/all', $settings->base_url));
+}
+$range = $params[1];
+$lapse = isset($params[2]) ? $params[2] : null;
+$seconds = 0;
+
+switch ($range) {
+	case 'week':
+		if ($lapse != 1) {
+			redir(sprintf('%srandom/week/1', $settings->base_url));
+		}
+		$seconds = 604800; /* 3600 * 24 * 7 */
+		break;
+	case 'month':
+		if ($lapse != 1 && $lapse != 3 && $lapse != 6) {
+			redir(sprintf('%srandom/month/1', $settings->base_url));
+		}
+		$seconds = $lapse * 2419200; /* 604800 * 4 */
+		break;
+	case 'year':
+		if ($lapse != 1) {
+			redir(sprintf('%srandom/year/1', $settings->base_url));
+		}
+		$seconds = 29030400; /* 2419200 * 12 */
+		breaK;
+}
+
+$selected->oneweek = ($seconds == 604800) ? $beacon : null;
+$selected->onemonth = ($seconds == 2419200) ? $beacon : null;
+$selected->threemonths = ($seconds == 3 * 2419200) ? $beacon : null;
+$selected->sixmonths = ($seconds == 6 * 2419200) ? $beacon : null;
+$selected->oneyear = ($seconds == 29030400) ? $beacon : null;
+$selected->all = ($seconds == 0) ? $beacon : null;
+
+$vars = compact('selected');
+$filterer = Haanga::Load('random.html', $vars, true);
+
+$diff = ($seconds == 0) ? 0 : (time() - $seconds);
+
 /* !!TODO */
 if ($db->type == 'mysql') {
 	$query = sprintf('SELECT %s FROM quotes WHERE quotes.db = :db
-		AND status = \'approved\' AND hidden = 0 ORDER BY RAND() LIMIT %d',
+		AND status = \'approved\' AND hidden = 0 AND timestamp > :timestamp
+		ORDER BY RAND() LIMIT %d',
 		Quote::READ, $settings->page_size);
 } elseif ($db->type == 'sqlite') {
 	$query = sprintf('SELECT %s FROM quotes WHERE quotes.db = :db
-		AND status = \'approved\' AND hidden = 0 ORDER BY RANDOM() LIMIT %d',
+		AND status = \'approved\' AND hidden = 0 AND timestamp > :timestamp
+		ORDER BY RANDOM() LIMIT %d',
 		Quote::READ, $settings->page_size);
 }
+
 $quotes = $db->get_results($query, array(
-	array(':db', $settings->db, PDO::PARAM_STR)
+	array(':db', $settings->db, PDO::PARAM_STR),
+	array(':timestamp', $diff, PDO::PARAM_INT)
 ));
 
 if (!$quotes) {
@@ -38,11 +86,16 @@ if (!$quotes) {
 }
 
 $html->do_header(_('Random quotes'));
-
+$html->output .= $filterer;
 $quote = new Quote();
 $odd = true;
+$shown = array();
 foreach ($quotes as $this_quote) {
 	$quote->read($this_quote);
+	if (in_array($quote->id, $shown)) {
+		continue;
+	}
+	$shown[] = $quote->id;
 	$quote->output($odd);
 	$odd = !$odd;
 }
