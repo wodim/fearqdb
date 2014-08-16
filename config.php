56960c240ff2f4180cc1114a2ad5d2c624279f5b
wodim <neikokz@gmail.com>
2014-08-17 00:20:15 +0200

    new config.php skeleton

--- config.php
+++ config.php
@@ -17,6 +17,9 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/* Important: it's recommended to make a copy of this file called "local.php"
+	and edit that file instead of this one, just in case. */
+
 /* ---------- DB ---------- */
 /* mysql or sqlite */
 $config['db']['type'] = null;
@@ -40,9 +43,48 @@ $config['db']['name'] = null;
 $config['db']['socket'] = null;
 $config['db']['host'] = null;
 
-/* ---------- MEMCACHE ---------- */
-$config['memcache']['enabled'] = false;
-$config['memcache']['server'] = null;
-$config['memcache']['port'] = null;
-$config['memcache']['prefix'] = 'fearqdb';
-$config['memcache']['debug'] = false;
+$sites = Array();
+
+$sites[0] = Array();
+/* domain. Example: qdb.es */
+$sites[0]['domain'] = '';
+/* base url. Most probably, / (don't touch it if unsure) */
+$sites[0]['base_url'] = '/';
+/* a private site key, used to generate cookies and other things.
+   use a random string. */
+$sites[0]['site_key'] = '';
+/* language of the site, such as 'en' or 'es' */
+$sites[0]['lang'] = 'en';
+/* locale of the site */
+$sites[0]['locale'] = 'es_ES.utf8';
+/* collation for the database */
+$sites[0]['collate'] = 'utf8_english_ci';
+/* show the ip address of those who sent the quotes to all users.
+   if it's false, they will be shown to registered users anyway. */
+$sites[0]['ip_show'] = '';
+/* google analytics code. leave blank if you don't use ga */
+$sites[0]['analytics_code'] = '';
+/* a different url for the statics folder. leave blank if you don't need it */
+$sites[0]['statics_url'] = '';
+/* the title of this site */
+$sites[0]['title'] = '';
+/* text shown at the footer */
+$sites[0]['footer'] = '';
+/* privacy level of the quotes. possible values:
+   -1: all quotes are shown, even the hidden ones
+    0: normal
+    1: all quotes are hidden, but nick/date are visible
+    2: you have to be logged in to actually read any quote
+   the second variable applies to bots such as the google crawler */
+$sites[0]['privacy_level'] = 0;
+$sites[0]['privacy_level_for_bots'] = 0;
+/* amount of quotes per page */
+$sites[0]['page_size'] = 10;
+/* allow or disallow robots with robots.txt */
+$sites[0]['robots'] = 'allow';
+/* push server used for sending topics from irc to the site */
+$sites[0]['push_enabled'] = false;
+$sites[0]['push_url'] = '';
+$sites[0]['push_params'] = '';
+/* extra css to insert in all pages */
+$sites[0]['extra_css'] = '';
